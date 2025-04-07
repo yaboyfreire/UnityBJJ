b9420b90cd5cf1ed5af6ff924285b07632f3085b
yaboyfreire <rfreire750@gmail.com>
2025-04-07 17:00:35 +0100

    GenericCompetitionAndTemplatePage

--- VSCode/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const hamburger = document.querySelector(".hamburger");
-    const menuList = document.querySelector(".menu-list");
-
-    hamburger.addEventListener("click", function() {
-        menuList.classList.toggle("show-menu");
-    });
-});
