6c6a6d5073b0dd2dfd6a215800384f5c343b5f1b
MartimConceicao <martimconceicao219@gmail.com>
2025-04-27 19:53:17 +0100

    teste

--- VSCode/HomePage.js
+++ VSCode/HomePage.js
@@ -13,9 +13,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const dropdownContent = document.createElement('div');
     dropdownContent.className = 'dropdown-content';
     dropdownContent.innerHTML = ` 
-        <a href="#">My Account</a>
-        <a href="#">Payments</a>
+        <a href="desempenhoac.html">My Account</a>
+        <a href="pagamentos.html">Payments</a>
         <a href="#">Progress</a>
+        <a href="medalsPage.html">Medals</a>
+        <a href="checkatendennce.html">Attendace</a>
         <a href="Competitions.html">Upcoming Competitions</a>
         <a href="#" class="logout-link">Logout</a>
     `;
@@ -113,6 +115,22 @@ document.addEventListener('DOMContentLoaded', function() {
     function handleMembershipClick(e) {
         // If the clicked target is "Upcoming Competitions", don't prevent the default behavior
         if (e.target.tagName === 'A' && e.target.getAttribute('href') === 'Competitions.html') {
+            return; // Let the link work
+        }
+
+        if (e.target.tagName === 'A' && e.target.getAttribute('href') === 'desempenhoac.html') {
+            return; // Let the link work
+        }
+
+        if (e.target.tagName === 'A' && e.target.getAttribute('href') === 'medalsPage.html') {
+            return; // Let the link work
+        }
+
+        if (e.target.tagName === 'A' && e.target.getAttribute('href') === 'checkatendennce.html') {
+            return; // Let the link work
+        }
+
+        if (e.target.tagName === 'A' && e.target.getAttribute('href') === 'pagamentos.html') {
             return; // Let the link work
         }
     
